1f71bd1bbe5b2f0826324b5beec0aefd84ceefd9
Andre Rodrigues <andrecomerciale@gmail.com>
2022-07-14 18:44:11 -0300

    feat: created the project

---
+++ Africa_SwiftUI/View/InsetGalleryView.swift
@@ -0,0 +1,39 @@
+//
+//  InsetGalleryView.swift
+//  Africa
+//
+//  Created by André Almeida on 2022-07-10.
+//
+
+import SwiftUI
+
+struct InsetGalleryView: View {
+    //MARK: - PROPERTIES
+    var animal: Animal
+    
+    
+    //MARK: - BODY
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 15) {
+                ForEach(animal.gallery, id: \.self) { item in
+                    Image(item)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 200)
+                    .cornerRadius(12)
+                }
+            }//: HSTACK
+        }
+    }
+}
+
+struct InsetGalleryView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        InsetGalleryView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
